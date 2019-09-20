8ad78dc4cf892e94720ac1020210d5685503bb71
Vincent van Proosdij <mini32@gmail.com>
2019-09-20 16:16:05 +0200

    init

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Naos.Sample
 A mildly opiniated modern cloud service architecture blueprint + reference implementation
+
+docker network create naos-network
+docker-compose -f .\services.yml -f .\services-local.yml up -d
---
+++ scripts/git-clone.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+REPOSITORIES=(Naos.Sample.ApiGateway Naos.Sample.Services.Availability Naos.Sample.Services.Customers Naos.Sample.Services.Deliveries Naos.Sample.Services.Identity Naos.Sample.Services.Operations Naos.Sample.Services.Orders Naos.Sample.Services.OrderMaker Naos.Sample.Services.Parcels Naos.Sample.Services.Pricing Naos.Sample.Services.Vehicles)
+
+for REPOSITORY in ${REPOSITORIES[*]}
+do
+	 echo ========================================================
+	 echo Cloning the repository: $REPOSITORY
+	 echo ========================================================
+	 REPO_URL=https://github.com/vip32/$REPOSITORY.git
+	 git clone $REPO_URL
+	 cd $REPOSITORY && cd ..
+done
