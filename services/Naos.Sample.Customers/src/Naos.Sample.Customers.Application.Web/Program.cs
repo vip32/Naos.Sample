265d4e33916f723138eb312e6c6717a38a0353be
Vincent van Proosdij <mini32@gmail.com>
2019-09-23 09:46:45 +0200

    init

--- services/Naos.Sample.Customers/src/Naos.Sample.Customers.Application.Web/Program.cs
+++ services/Naos.Sample.Customers/src/Naos.Sample.Customers.Application.Web/Program.cs
@@ -1,14 +1,7 @@
 ﻿namespace Naos.Sample.Customers.Application.Web
 {
-    using System;
-    using System.Collections.Generic;
-    using System.IO;
-    using System.Linq;
-    using System.Threading.Tasks;
     using Microsoft.AspNetCore;
     using Microsoft.AspNetCore.Hosting;
-    using Microsoft.Extensions.Configuration;
-    using Microsoft.Extensions.Logging;
 
     public class Program
     {
--- services/Naos.Sample.Orders/src/Naos.Sample.Orders.Application.Web/Controllers/ValuesController.cs
+++ services/Naos.Sample.Orders/src/Naos.Sample.Orders.Application.Web/Controllers/ValuesController.cs
@@ -1,9 +1,6 @@
 ﻿namespace Naos.Sample.Orders.Application.Web.Controllers
 {
-    using System;
     using System.Collections.Generic;
-    using System.Linq;
-    using System.Threading.Tasks;
     using Microsoft.AspNetCore.Mvc;
 
     [Route("api/[controller]")]
